fb0b50869a81669fe888cd2ff407874ffabf5f54
ZiadMostafaGit <mygithub2003@gmail.com>
2024-10-04 14:00:34 +0300

    Initial push

---
+++ target/debug/deps/rustByPractice-72f1cb1493fee285.d
@@ -0,0 +1,5 @@
+/home/ziad/github/rustByPractice/target/debug/deps/rustByPractice-72f1cb1493fee285: src/main.rs
+
+/home/ziad/github/rustByPractice/target/debug/deps/rustByPractice-72f1cb1493fee285.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rustByPractice.d
@@ -0,0 +1,1 @@
+/home/ziad/github/rustByPractice/target/debug/rustByPractice: /home/ziad/github/rustByPractice/src/main.rs
