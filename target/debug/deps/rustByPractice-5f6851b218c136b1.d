e20a3008b15ab25f9be09ee7e98b299b70456deb
ZiadMostafaGit <mygithub2003@gmail.com>
2024-10-14 02:57:02 -0700

    update

---
+++ target/debug/deps/rustByPractice-5f6851b218c136b1.d
@@ -0,0 +1,5 @@
+C:\Users\ziad\github\rustByPractice\target\debug\deps\librustByPractice-5f6851b218c136b1.rmeta: src/main.rs
+
+C:\Users\ziad\github\rustByPractice\target\debug\deps\rustByPractice-5f6851b218c136b1.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rustByPractice-ff4ebd071d5eedf6.d
@@ -0,0 +1,5 @@
+C:\Users\ziad\github\rustByPractice\target\debug\deps\librustByPractice-ff4ebd071d5eedf6.rmeta: src/main.rs
+
+C:\Users\ziad\github\rustByPractice\target\debug\deps\rustByPractice-ff4ebd071d5eedf6.d: src/main.rs
+
+src/main.rs:
